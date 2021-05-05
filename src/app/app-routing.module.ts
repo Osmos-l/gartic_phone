5b19c9157eafdbe67e90d6d83ac71dc50b275244
Adrien <garrousteadrien@gmail.com>
2021-05-05 11:04:26 +0200

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LobbyComponent } from './game/lobby/lobby.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'lobby/:id', component: LobbyComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
